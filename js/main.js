1148007a8e434094fef4e26584ce141e0381b001
SoldatdOR05 <ethan.paleyron@gmail.com>
2023-09-10 17:15:46 +0200

    bug beast score

--- js/main.js
+++ js/main.js
@@ -3,22 +3,18 @@ const bestScore = document.querySelector("#bestScore");
 const sec = document.querySelector("#sec");
 const ms = document.querySelector("#ms");
 
-// Select Sec
-const select = document.querySelector("select");
-sec.innerText = select.value;
-select.addEventListener("change", () => {
-  sec.innerText = select.value;
-});
-
 // Counteur
-const counteur = document.querySelector("#testCPS");
-let timerActif = "";
+const counteur = document.querySelector(".testCPS");
+let timer = "";
 counteur.addEventListener("click", () => {
+  select.disabled = "true";
   counteur.innerText++;
+
   // Timer
-  if (timerActif !== "GO") {
+  if (timer === "") {
+    timer = "start";
     setInterval((e) => {
-      if (sec.innerText > 0) {
+      if (sec.innerText > -1) {
         ms.innerText--;
         if (ms.innerText < 0) {
           sec.innerText--;
@@ -26,30 +22,43 @@ counteur.addEventListener("click", () => {
         }
       } else {
         // End of timer
-        counteur.style.display = "none";
-        ms.style.display = "none";
-        sec.style.display = "none";
-        document.querySelector(".time").innerText = "Finish";
+        clearInterval(e);
+        counteur.disabled = "true";
+        // Your record
         previousPart.innerText = counteur.innerText;
-        savePreviousPart();
+        valueSec.innerText = select.value;
+        document.querySelector(".timer").innerText = "Finish";
+        document.querySelector("#blurring").classList.add("blurring");
+        document.querySelector(".p_section").style.display = "flex";
+        // Best record
         if (bestScore.innerText < counteur.innerText) {
           bestScore.innerText = counteur.innerText;
+          console.log(bestScore);
           saveBestScore();
         }
-        clearInterval(e);
+        savePreviousPart();
       }
     }, 10);
   }
-  if (timerActif === "") {
-    timerActif = "GO";
-  }
 });
 
-// location.reload();
+// Select Sec
+const select = document.querySelector("select");
+sec.innerText = select.value;
+select.addEventListener("change", () => {
+  sec.innerText = select.value;
+});
 
 // reboot
-document.querySelector("#restart").addEventListener("click", () => {
-  location.reload();
+document
+  .querySelector(".restart")
+  .addEventListener("click", () => location.reload());
+document
+  .querySelector(".replay")
+  .addEventListener("click", () => location.reload());
+document.querySelector(".close").addEventListener("click", () => {
+  document.querySelector(".p_section").style.display = "none";
+  document.querySelector("#blurring").classList.remove("blurring");
 });
 
 // LocalStorage
